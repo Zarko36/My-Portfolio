3ce955270d6d4ce6195452593421bbc495b9dab6
Joel Muniz <Jmuni017@ucr.edu>
2024-05-27 17:16:16 -0700

    update pdf naming

--- components/About.jsx
+++ components/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import Resume from '@/components/resume.pdf';
+import Resume from '../components/Resume.pdf';
 
 const animate = {
   start: {
